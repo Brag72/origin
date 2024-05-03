3a0302c9f2a8bc8b1011bdccd398acf443769c2d
Andrew Cheremukhin <z-7277@yandex.ru>
2024-05-03 23:21:46 +0300

    Add Lesson3

---
+++ Lesson3/Task1/Task1/Task1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+class Adress
+{
+    std::string city;
+    std::string street;
+    int nHome;
+    int nAppart;
+
+public:
+
+    Adress(std::ifstream &fin)
+    {
+        fin >> city;
+        fin >> street;
+        fin >> nHome;
+        fin >> nAppart;
+    }
+    Adress(){}
+   
+    void swaps(Adress* arr,int arr_size)
+    {
+        int i = 0;
+        int j = arr_size - 1;
+
+        while (i < j)
+        {
+            std::swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    void fileStream(std::ofstream &fout)
+    {
+        fout << city << ", " << street << ", " << nHome << ", " << nAppart << std::endl;
+    }
+};
+int main()
+{
+    std::ifstream fin("in.txt");
+    int arr_size;
+    fin >> arr_size;
+    
+    std::ofstream fout("out.txt");
+   
+    Adress* arr = new Adress[arr_size];
+    
+
+    for (int i = 0; i < arr_size; ++i)
+    {
+        Adress adress(fin);
+        arr[i] = adress;
+    }    
+    fin.close();
+
+    arr->swaps(arr, arr_size);
+    
+    fout << arr_size << std::endl;
+    
+   for (int i = 0; i < arr_size; ++i)
+    {
+        arr[i].fileStream(fout);
+    }
+    fout.close();
+    delete[] arr;
+}   
---
+++ Lesson3/Task2/Task2/Task2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+class Adress
+{
+    std::string city;
+    std::string street;
+    int nHome;
+    int nAppart;
+
+public:
+
+    Adress(std::ifstream& fin)
+    {
+        fin >> city;
+        fin >> street;
+        fin >> nHome;
+        fin >> nAppart;
+    }
+    Adress() {}
+
+    void fileStream(std::ofstream& fout)
+    {
+        fout << city << ", " << street << ", " << nHome << ", " << nAppart << std::endl;
+    }
+    std::string getCity()
+    {
+        return city;
+    }
+};
+int main()
+{
+    std::ifstream fin("in.txt");
+    int arr_size;
+    fin >> arr_size;
+
+    std::ofstream fout("out.txt");
+
+    Adress* arr = new Adress[arr_size];
+    
+    Adress temp;
+    
+    for (int i = 0; i < arr_size; ++i)
+    {
+        Adress adress(fin);
+        arr[i] = adress;
+    }
+    fin.close();
+
+    for (int j = 0; j < arr_size; ++j)
+    {
+        for (int i = 0; i < arr_size - 1; i++)
+        {
+            if (arr[i].getCity() > arr[i + 1].getCity())
+            {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    fout << arr_size << std::endl;
+
+    for (int i = 0; i < arr_size; ++i)
+    {
+        arr[i].fileStream(fout);
+    }
+    fout.close();
+    delete[] arr;
+}
